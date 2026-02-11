748df5377177b3b85311bef228404e51a596d2b3
Leif W. Sogge <144378178+GuardianNinja@users.noreply.github.com>
2026-02-11 10:11:05 -0500

    Add IdentityToken interface and related types

---
+++ src/model/Identity.ts
@@ -0,0 +1,18 @@
+export type Scale = 'G' | 'N' | 'L'; // Global / National / Local
+export type Domain = 'GO' | 'AO' | 'SO'; // Ground / Air / Space
+
+export interface IdentityToken {
+  raw: string;
+
+  scale: Scale;          // G / N / L
+  state: string;         // FL, CA, TX, etc.
+  fullName: string;      // LEIFWILLIAMSOGGE
+  domain: Domain;        // GO / AO / SO
+  facility: string;      // MCO, VBG, CCC, etc.
+  mission: string;       // TRACON, LAUNCHWINDOW, SFN, etc.
+
+  // Optional: derived fields
+  isGlobal: boolean;
+  isNational: boolean;
+  isLocal: boolean;
+}
