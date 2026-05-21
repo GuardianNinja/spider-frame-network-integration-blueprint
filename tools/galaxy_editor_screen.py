fbf24ce49a7d63c927cbb3adf69568fb6a8ed099
Leif W. Sogge <144378178+GuardianNinja@users.noreply.github.com>
2026-05-21 08:28:41 -0400

    Update galaxy_editor_screen.py

--- tools/galaxy_editor_screen.py
+++ tools/galaxy_editor_screen.py
@@ -1,20 +1,10 @@
-import pygame
-import math
-from tools.galaxy_net import GalaxyNet
-
-
-def distance(a, b):
-    ax, ay = a
-    bx, by = b
-    return math.hypot(ax - bx, ay - by)
-
-
 class GalaxyEditorScreen:
     def __init__(self, screen):
         self.screen = screen
         self.net = GalaxyNet()
         self.next_id = 1
         self.selected = None
+        self.launch_node = None  # NEW
 
     def handle_click(self, pos):
         # select or connect
@@ -25,25 +15,10 @@ class GalaxyEditorScreen:
                     self.selected = None
                 else:
                     self.selected = node
+                    self.launch_node = node  # NEW: selecting a node marks it for launch
                 return
 
         # otherwise create node
         node_id = f"N{self.next_id}"
         self.next_id += 1
         self.net.add_node(node_id, f"Node {node_id}", pos)
-
-    def draw(self):
-        self.screen.fill((0, 0, 20))
-
-        # draw connections
-        for node in self.net.nodes.values():
-            for cid in node.connections:
-                other = self.net.nodes[cid]
-                pygame.draw.line(self.screen, (80, 120, 200), node.position, other.position, 1)
-
-        # draw nodes
-        for node in self.net.nodes.values():
-            color = (200, 240, 255)
-            if self.selected == node:
-                color = (255, 220, 140)
-            pygame.draw.circle(self.screen, color, node.position, 8)
